39fb0449c831fd56fc9cf27c738ecb21e927528e
matheusyanmonteiro <matheusyanmonteiro@gmail.com>
2022-12-14 13:40:56 -0300

    update swagger for 0. 3.1

--- src/swagger/doc.ts
+++ src/swagger/doc.ts
@@ -1,4 +1,3 @@
 export const description =
-  'Nosso  projeto  final,  chamado  Descomplication,  tem  como  objetivo  facilitar  o  processo  de onboarding  de  novos  funcionários  da  área  de  Engenharia  da  Descomplica,  além  de  facilitar  a troca de desenvolvedores entre diferentes squads.\n Esta pagína é a documentação do back end do projeto trazendo os tipos validos de rquests e as respostas que o sistema deverá proporcionar aos programadores e qualquer outro serviço que for acoplado com o mesmo.';
-
-export const version = '0.2.6';
+  'Nosso projeto final, chamado Descomplication, tem como objetivo facilitar o processo de onboarding de novos funcionários da área de Engenharia da Descomplica, além de facilitar a troca de desenvolvedores entre diferentes squads. Esta página possui a documentação do back-end do projeto, trazendo os tipos válidos de requests e as respostas que o sistema deverá proporcionar aos programadores e qualquer outro serviço que for acoplado com o back-end.';
+export const version = '0.3.1';
