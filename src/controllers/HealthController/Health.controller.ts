2e1160df16bbbb5062414d981bbcfaf5540eee1e
matheusyanmonteiro <matheusyanmonteiro@gmail.com>
2022-12-13 22:33:41 -0300

    fix/remove depencies at healthcheck

--- src/controllers/HealthController/Health.controller.ts
+++ src/controllers/HealthController/Health.controller.ts
@@ -1,18 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
-import { HealthCheckService, HealthCheck } from '@nestjs/terminus';
 import { IsPublic } from 'src/decorators/is-public.decorator';
 
 @ApiTags('Health')
 @Controller('health')
 @IsPublic()
 class HealthController {
-  constructor(private heath: HealthCheckService) {}
-
   @Get()
-  @HealthCheck()
   check() {
-    return this.heath.check([]);
+    return {
+      health: true,
+    };
   }
 }
 
--- src/modules/health.module.ts
+++ src/modules/health.module.ts
@@ -1,10 +1,8 @@
-import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
-import { TerminusModule } from '@nestjs/terminus';
 import { HealthController } from 'src/controllers/HealthController/Health.controller';
 
 @Module({
-  imports: [TerminusModule, HttpModule],
+  imports: [],
   controllers: [HealthController],
   providers: [],
 })
